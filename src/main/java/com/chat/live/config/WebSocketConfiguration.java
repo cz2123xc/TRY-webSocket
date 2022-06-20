9b4293c5751e3daf22b6a982ed72e923697c425d
지금이야 <cz2123xc@gmail.com>
2022-06-21 02:12:38 +0900

    subscribe not working

--- src/main/java/com/chat/live/config/WebSocketConfiguration.java
+++ src/main/java/com/chat/live/config/WebSocketConfiguration.java
@@ -27,11 +27,14 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // 1. 핸드쉐이크 통신 담당할 엔드포인트 지정
+        log.info("엔드포인트 진입");
         registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS(); // withSockJS() 는 삭제했다. 이유는 프론트에서 SockJS로 통신하지 않으면 오류가 난다.
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
+        log.info("메시지 브로커 설정");
+        log.info("config: {}", config);
         // 2.  Application 내부에서 사용할 path 지정
         config.setApplicationDestinationPrefixes("/app"); // (pub) Client 에서 SEND 되는 메시지를 처리 (@MessageMapping @Controller 로 라우팅 되는 메시지)
         config.enableSimpleBroker("/topic", "/queue"); // (sub) 해당하는 경로를 SUBSCRIBE 하는 Client 에게 메시지를 간단하게 전달 queue = 1:1 메시지 (내장된 브로커 사용)
--- src/main/java/com/chat/live/controller/ChatController.java
+++ src/main/java/com/chat/live/controller/ChatController.java
@@ -20,8 +20,10 @@ public class ChatController {
      *
      * /pub/chat/join 로 전달된 메시지를 sub(/topic or /queue)/chat 로 전달한다.
      */
-    @MessageMapping("/chat/join") // 실제 경로는 앞에 app, topic, queue 가 붙여진다. 현재는 publish, subscribe 도 붙을 수 있음
-    public void join(Message message) { // ChatMessage 는 채팅 메시지를 담은 엔티티.
+
+    @MessageMapping("/subscribe") // 실제 경로는 앞에 app, topic, queue 가 붙여진다. 현재는 publish, subscribe 도 붙을 수 있음
+    public void SubscribeMappingController(Message message) { // ChatMessage 는 채팅 메시지를 담은 엔티티.
+        log.info("join 컨트롤러 진입");
         log.info("join: {}", message);
         message.setMessage("[입장] " + message.getSender());
         template.convertAndSend("/topic/chat/room/" + message.getChannelId(), message); // 1:N 전송.(입장 메시지 이므로)
@@ -31,8 +33,8 @@ public class ChatController {
      *
      * /pub/chat/message => /topic/chat/room/{channelId}
      */
-    @MessageMapping("/chat/message")
-    public void message(Message message) {
+    @MessageMapping("/request")
+    public void handleMessageWithoutResponse(Message message) {
         template.convertAndSend("/topic/chat/room/" + message.getChannelId(), message); // 1:N 전송.(귓말이 아니므로)
     }
 
